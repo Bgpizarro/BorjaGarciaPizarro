5465125edcf985ec12663da60d9c03898fcceb8e
Bgpizarro <borjagarpiz@gmail.com>
2023-05-09 11:58:34 +0200

    Clase Tres incompleta

---
+++ ClaseTres/src/Program.java
@@ -0,0 +1,93 @@
+import java.io.*;
+import java.util.*;
+
+import indra.utils.Consola;
+import java.util.Scanner;
+
+public class Program {
+
+////	public static void main(String[] args)  {
+////	try {
+////		//Como el main no tiene throws, tengo que usar un try catch
+////		Program.ejemploBufferedReaderConThrows();
+////	} catch (IOException e) {
+////		e.printStackTrace();
+////	}
+////}
+//
+//	public static void main(String[] args)  {
+//		
+//	//Es numero
+////		try(Scanner in= new Scanner(System.in)){
+////			System.out.println("Ingresa número");
+////			String num=in.nextLine();
+////			if(esNumerico(num)){
+////				System.out.println("Es numero");
+////			}else {
+////				System.out.println("No es un número");
+////			}
+////		}
+//		
+//	//Leer en bucle hasta introducir número
+//		//System.out.println("Tu número es: "+ readInt());
+//		
+//	//Con sobrecarga
+//		int num=readIntConTexto("Ingrese un número");
+//		System.out.println("Su número es: "+num);
+//	}
+	
+	public static void Random() {
+		System.out.println("Tienes 5 intentos");
+		System.out.println("Adivina el número de 1 a 100");
+		Random r= new Random();
+		int azar=r.nextInt(1,100);
+		int intentos=5, num=-1;
+					
+		do{
+			
+			num=Consola.readInt();
+				
+			if(num>azar) {
+				System.out.println("el tuyo es mas grande");
+			}else if(num<azar) {
+				System.out.println("el tuyo es mas pequeño");
+			}else {
+				System.out.println("Acertaste");
+				return;
+			}
+			intentos--;
+		}while(intentos>0);
+	}
+	
+	public static void adivinanzas() {
+		//TODO
+	}
+	
+	public static void primo() {
+		int resto=0;
+		int num=Consola.readInt();
+		for(int i=num-1;i>1;i--) {
+			resto=num%i;
+			if(resto==0) {
+				System.out.println("No es primo");
+				return;
+			}
+		}
+		System.out.println("Es primo");
+	}
+	
+	public static void main(String[] args) {
+//		int num=Consola.readIntConTexto("Ingrese un número");
+//		System.out.println("Su número es: "+ num);
+		
+		
+		//Random();
+		
+		
+		//dado un numero si es primo
+		
+		primo();
+		
+		
+	}
+}
---
+++ ClaseTres/src/indra/utils/Consola.java
@@ -0,0 +1,114 @@
+package indra.utils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Consola {
+	
+	public static void ejemploBufferedReaderConThrows() throws IOException{
+		
+		// TODO Auto-generated method stub
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		try {
+				
+			
+			System.out.println("Ingresa tu nombre: ");
+			
+			//try {
+				String nombre =reader.readLine();
+				System.out.println("Ingresa tu edad: ");
+				String edad =reader.readLine();
+				int age= Integer.parseInt(edad);
+				
+				System.out.println("Hola "+ nombre);
+				System.out.println("Cuya edad es "+ age);
+			
+			//} catch(IOException e) {
+				//System.out.println(e.getMessage());
+			//}
+			
+		} finally {
+			System.out.println("Cerrando el BufferedReader");
+			reader.close();
+		}
+	}
+	
+	public static void ejemploBufferedReaderSinThrows() {
+		
+		// TODO Auto-generated method stub
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		try {
+				
+			
+			System.out.println("Ingresa tu nombre: ");
+			
+			try {
+				String nombre =reader.readLine();
+				System.out.println("Ingresa tu edad: ");
+				String edad =reader.readLine();
+				int age= Integer.parseInt(edad);
+				
+				System.out.println("Hola "+ nombre);
+				System.out.println("Cuya edad es "+ age);
+			
+			} catch(IOException e) {
+				System.out.println(e.getMessage());
+			}
+			
+		} finally {
+			System.out.println("Cerrando el BufferedReader");
+			try {
+			reader.close();
+			} catch(IOException e) {
+				System.err.println(e.getMessage());
+			}
+		}
+	}
+	
+
+	public static void ejemploScanner() {
+		try(Scanner in= new Scanner(System.in)){
+			System.out.println("Su nombre es...");
+			String nombre=in.nextLine();
+			System.out.println("Su edad es...");
+			int edad=in.nextInt();
+			System.out.println("Hola "+nombre+" de "+edad+" años");
+		}//hace él solo un close24
+	}
+	
+	public static boolean esNumerico(String numero) {
+		try {
+			Integer.parseInt(numero);
+			return true;
+			}catch(NumberFormatException e){
+				return false;
+			}
+	}
+	
+	public static int readInt() {
+		String num=null;
+		try(Scanner in= new Scanner(System.in)){	
+			do {
+				System.out.println("Ingresa número");
+				num=in.nextLine();
+			}while(!esNumerico(num));
+			
+			return Integer.valueOf(num);
+		}
+	}
+	
+	public static int readIntConTexto(String prompt) {
+		String num=null;
+		try(Scanner in= new Scanner(System.in)){	
+			do {
+				System.out.println(prompt);
+				num=in.nextLine();
+			}while(!esNumerico(num));
+			System.out.println("Buen número");
+			return Integer.valueOf(num);
+		}
+	}
+	
+}
